d0c5ac70662479b0d291ed39c64529e237ca2a88
giver <giverliu@gmail.com>
2023-06-06 21:53:08 +0800

    clean up logs

--- Plugins/GetJoystickUnreal/Source/GetJoystickUnreal/Private/GetJoystickUnreal.cpp
+++ Plugins/GetJoystickUnreal/Source/GetJoystickUnreal/Private/GetJoystickUnreal.cpp
@@ -12,7 +12,6 @@ FGetJoystickUnrealModule::FGetConfigContentCompleteDelegate FGetJoystickUnrealMo
 void FGetJoystickUnrealModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	UE_LOG(LogTemp, Warning, TEXT("JoystickUnreal: GetJoystickUnreal Startup Module!"));
 }
 
 void FGetJoystickUnrealModule::ShutdownModule()
@@ -23,14 +22,12 @@ void FGetJoystickUnrealModule::ShutdownModule()
 
 void FGetJoystickUnrealModule::FetchConfigContent(const TArray<FString> ContentIDs)
 {
-	UE_LOG(LogTemp, Warning, TEXT("JoystickUnreal: FetchConfigContent"));
 	GetJoystickUnrealServices::GetConfigContentCompleteDelegate.BindStatic(&OnFetchConfigContentComplete);
 	GetJoystickUnrealServices::PerformHttpPost(ContentIDs);
 }
 
 void FGetJoystickUnrealModule::FetchCatalogContent()
 {
-	UE_LOG(LogTemp, Warning, TEXT("JoystickUnreal: FetchCatalogContent"));
 	GetJoystickUnrealServices::GetCatalogCompleteDelegate.BindStatic(&OnFetchCatalogContentComplete);
 	GetJoystickUnrealServices::PerformHttpGetCatalog();
 }
@@ -38,20 +35,16 @@ void FGetJoystickUnrealModule::FetchCatalogContent()
 void FGetJoystickUnrealModule::SetRuntimeEnvironmentAPIKey(FString apiKey)
 {
 	UGetJoystickUnrealSettings::SetCurrentEnvironmentAPIKey(apiKey);
-
-	UE_LOG(LogTemp, Warning, TEXT("JoystickUnreal: SetRuntimeEnvironmentAPIKey: %s"), *apiKey);
 }
 
 void FGetJoystickUnrealModule::OnFetchConfigContentComplete(bool Succeed, FString ResponseJsonData)
 {
-	UE_LOG(LogTemp, Warning, TEXT("JoystickUnreal: OnFetchConfigContentComplete: %s"), *ResponseJsonData);
 	GetJoystickUnrealServices::GetConfigContentCompleteDelegate.Unbind();
 	FGetJoystickUnrealModule::GetConfigContentCompleteDelegate.ExecuteIfBound(Succeed, ResponseJsonData);
 }
 
 void FGetJoystickUnrealModule::OnFetchCatalogContentComplete(bool Succeed, FString ResponseJsonData)
 {
-	UE_LOG(LogTemp, Warning, TEXT("JoystickUnreal: OnFetchCatalogContentComplete: %s"), *ResponseJsonData);
 	GetJoystickUnrealServices::GetCatalogCompleteDelegate.Unbind();
 	FGetJoystickUnrealModule::GetCatalogCompleteDelegate.ExecuteIfBound(Succeed, ResponseJsonData);
 }
